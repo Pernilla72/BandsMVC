db5ad5b731d512a64e51703dfe95ac7c7627f716
PiiKay <144244080+Pernilla72@users.noreply.github.com>
2024-10-29 15:34:46 +0100

    Skapat klassen Band, med tre props

---
+++ BandsMVC/Models/Band.cs
@@ -0,0 +1,10 @@
+﻿namespace BandsMVC.Models
+{
+    public class Band
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
