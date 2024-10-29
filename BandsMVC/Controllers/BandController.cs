aadb4afad28ac803966330c434f35052514c6dda
PiiKay <144244080+Pernilla72@users.noreply.github.com>
2024-10-29 15:31:57 +0100

    skapat mapparna  för MVS mönstert

---
+++ BandsMVC/Controllers/BandController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BandsMVC.Controllers
+{
+    public class BandController : Controller
+    {
+        static DataService _dataService = new DataService();
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("/Details/{id}")]
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
