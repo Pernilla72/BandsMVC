4916a5bfddbdf1afa60818655995f9852c1e6051
PiiKay <144244080+Pernilla72@users.noreply.github.com>
2024-10-29 15:54:18 +0100

    uppdaterat DataService

--- BandsMVC/Models/Band.cs
+++ BandsMVC/Models/Band.cs
@@ -2,8 +2,16 @@
 
 public class Band
 {
+    public Band(int id, string name, string description)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+    }
+
     public int Id { get; set; }
     public String Name { get; set; }
     public string Description { get; set; }
 
+    
 }
--- BandsMVC/Models/DataService.cs
+++ BandsMVC/Models/DataService.cs
@@ -2,19 +2,16 @@
 
 public class DataService
 {
-    List<FavoriteBand> favoriteBands = [
+    List<Band> bands = [
 
-    new FavoriteBand (1, "Amon Amart", "Swedish Viking death Metal band, from Tumba"),
-    new FavoriteBand (2, "Rolling Stones", "Classic rock & Roll band from forever"),
-    new FavoriteBand (3, "Mora Träsk", "Epic musicgroup, fromed in 1971"),
+    new Band (1, "Amon Amart", "Swedish Viking death Metal band, from Tumba"),
+    new Band (2, "Rolling Stones", "Classic rock & Roll band from forever"),
+    new Band (3, "Mora Träsk", "Epic musicgroup, fromed in 1971"),
     ];
 
-    public FavoriteBand Get(int id)
+    public Band Get(int id)
     {
-        var ret = favoriteBands.FirstOrDefault(o => o.Id == id);
+        var ret = bands.FirstOrDefault(o => o.Id == id);
         return ret;
     }
-
-
-
 }
