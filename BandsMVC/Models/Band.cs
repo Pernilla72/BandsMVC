9cd70697c0c4f379826509f826fd7480133f0942
PiiKay <144244080+Pernilla72@users.noreply.github.com>
2024-10-29 15:49:46 +0100

    Spakade RataService och en lista av band.

--- BandsMVC/Models/Band.cs
+++ BandsMVC/Models/Band.cs
@@ -1,10 +1,9 @@
-﻿namespace BandsMVC.Models
-{
-    public class Band
-    {
-        public int Id { get; set; }
-        public String Name { get; set; }
-        public string Description { get; set; }
+﻿namespace BandsMVC.Models;
 
-    }
+public class Band
+{
+    public int Id { get; set; }
+    public String Name { get; set; }
+    public string Description { get; set; }
+
 }
